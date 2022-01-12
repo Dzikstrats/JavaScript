1f52a892ae6fcb3a6c38ca7db8a4d6b3e52de90e
Dzikstarts <imam@fahru.my.id>
2022-01-12 14:53:44 +0700

    all

---
+++ variable.js
@@ -0,0 +1,32 @@
+// let namaterakhir = "Hidayat";
+ 
+// console.log(namaterakhir)
+
+// let namadepan;
+
+// namadepan = "Muhammad Dzikry"
+
+// console.log(namadepan)
+
+// const uangjajan = 100
+
+// console.log(uangjajan)
+
+// uangjajantambahan = 210
+
+// console.log(uangjajantambahan)
+
+//kuis
+
+let firstname = "Muhammad Dizkry";
+let lastname = "Hidayat";
+let umur = 16;
+const a = "Belum Menikah";
+const b = "Sudah Menikah";
+
+let benar = a
+let salah = b
+
+console.log(firstname + lastname)
+console.log(umur)
+console.log(benar)
